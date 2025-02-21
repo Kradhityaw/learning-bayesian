2d2010ca355449fce49dde4ebddc8eb94d28d366
smkn1bondowoso <smkn1@gmail.com>
2025-02-21 19:08:45 +0700

    Menambahkan folder aplikasi desktop

--- BayesianUI/Form2.Designer.cs
+++ BayesianUI/Form2.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
@@ -60,18 +61,20 @@
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(401, 330);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(602, 508);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(368, 154);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(552, 237);
             this.flowLayoutPanel1.TabIndex = 27;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(402, 285);
+            this.label7.Location = new System.Drawing.Point(603, 438);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(107, 25);
+            this.label7.Size = new System.Drawing.Size(154, 37);
             this.label7.TabIndex = 26;
             this.label7.Text = "Posterior";
             // 
@@ -79,25 +82,28 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(130, 505);
+            this.label6.Location = new System.Drawing.Point(195, 777);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(0, 25);
+            this.label6.Size = new System.Drawing.Size(0, 37);
             this.label6.TabIndex = 25;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(24, 511);
+            this.label5.Location = new System.Drawing.Point(36, 786);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.Size = new System.Drawing.Size(103, 20);
             this.label5.TabIndex = 24;
             this.label5.Text = "Hasil Prediksi";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(27, 446);
+            this.button1.Location = new System.Drawing.Point(40, 686);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(314, 38);
+            this.button1.Size = new System.Drawing.Size(471, 58);
             this.button1.TabIndex = 23;
             this.button1.Text = "Submit";
             this.button1.UseVisualStyleBackColor = true;
@@ -107,17 +113,19 @@
             // 
             this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(135, 404);
+            this.comboBox4.Location = new System.Drawing.Point(202, 622);
+            this.comboBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(206, 21);
+            this.comboBox4.Size = new System.Drawing.Size(307, 28);
             this.comboBox4.TabIndex = 22;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(24, 408);
+            this.label4.Location = new System.Drawing.Point(36, 628);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.Size = new System.Drawing.Size(106, 20);
             this.label4.TabIndex = 21;
             this.label4.Text = "Jumlah Siswa";
             // 
@@ -125,17 +133,19 @@
             // 
             this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(135, 367);
+            this.comboBox3.Location = new System.Drawing.Point(202, 565);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(206, 21);
+            this.comboBox3.Size = new System.Drawing.Size(307, 28);
             this.comboBox3.TabIndex = 20;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(24, 371);
+            this.label3.Location = new System.Drawing.Point(36, 571);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.Size = new System.Drawing.Size(153, 20);
             this.label3.TabIndex = 19;
             this.label3.Text = "Media Pembelajaran";
             // 
@@ -143,17 +153,19 @@
             // 
             this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(135, 330);
+            this.comboBox2.Location = new System.Drawing.Point(202, 508);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(206, 21);
+            this.comboBox2.Size = new System.Drawing.Size(307, 28);
             this.comboBox2.TabIndex = 18;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 334);
+            this.label2.Location = new System.Drawing.Point(36, 514);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.Size = new System.Drawing.Size(99, 20);
             this.label2.TabIndex = 17;
             this.label2.Text = "Pemahaman";
             // 
@@ -161,17 +173,19 @@
             // 
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(135, 293);
+            this.comboBox1.Location = new System.Drawing.Point(202, 451);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(206, 21);
+            this.comboBox1.Size = new System.Drawing.Size(307, 28);
             this.comboBox1.TabIndex = 16;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 297);
+            this.label1.Location = new System.Drawing.Point(36, 457);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.Size = new System.Drawing.Size(100, 20);
             this.label1.TabIndex = 15;
             this.label1.Text = "Gaya Belajar";
             // 
@@ -193,15 +207,18 @@
             this.numberOfStudentsDataGridViewTextBoxColumn,
             this.learningMethodDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.criteriaBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 42);
+            this.dataGridView1.Location = new System.Drawing.Point(20, 65);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(775, 213);
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(1162, 328);
             this.dataGridView1.TabIndex = 14;
             // 
             // idDataGridViewTextBoxColumn
             // 
             this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
             this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
             this.idDataGridViewTextBoxColumn.Visible = false;
             // 
@@ -209,30 +226,35 @@
             // 
             this.learningStyleDataGridViewTextBoxColumn.DataPropertyName = "learningStyle";
             this.learningStyleDataGridViewTextBoxColumn.HeaderText = "learningStyle";
+            this.learningStyleDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.learningStyleDataGridViewTextBoxColumn.Name = "learningStyleDataGridViewTextBoxColumn";
             // 
             // initialUnderstandingDataGridViewTextBoxColumn
             // 
             this.initialUnderstandingDataGridViewTextBoxColumn.DataPropertyName = "initialUnderstanding";
             this.initialUnderstandingDataGridViewTextBoxColumn.HeaderText = "initialUnderstanding";
+            this.initialUnderstandingDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.initialUnderstandingDataGridViewTextBoxColumn.Name = "initialUnderstandingDataGridViewTextBoxColumn";
             // 
             // learningMediaDataGridViewTextBoxColumn
             // 
             this.learningMediaDataGridViewTextBoxColumn.DataPropertyName = "learningMedia";
             this.learningMediaDataGridViewTextBoxColumn.HeaderText = "learningMedia";
+            this.learningMediaDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.learningMediaDataGridViewTextBoxColumn.Name = "learningMediaDataGridViewTextBoxColumn";
             // 
             // numberOfStudentsDataGridViewTextBoxColumn
             // 
             this.numberOfStudentsDataGridViewTextBoxColumn.DataPropertyName = "numberOfStudents";
             this.numberOfStudentsDataGridViewTextBoxColumn.HeaderText = "numberOfStudents";
+            this.numberOfStudentsDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.numberOfStudentsDataGridViewTextBoxColumn.Name = "numberOfStudentsDataGridViewTextBoxColumn";
             // 
             // learningMethodDataGridViewTextBoxColumn
             // 
             this.learningMethodDataGridViewTextBoxColumn.DataPropertyName = "learningMethod";
             this.learningMethodDataGridViewTextBoxColumn.HeaderText = "learningMethod";
+            this.learningMethodDataGridViewTextBoxColumn.MinimumWidth = 8;
             this.learningMethodDataGridViewTextBoxColumn.Name = "learningMethodDataGridViewTextBoxColumn";
             // 
             // criteriaBindingSource
@@ -241,25 +263,27 @@
             // 
             // menuStrip1
             // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.settingsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1200, 35);
             this.menuStrip1.TabIndex = 28;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // settingsToolStripMenuItem
             // 
             this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
-            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(92, 29);
             this.settingsToolStripMenuItem.Text = "Settings";
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 570);
+            this.ClientSize = new System.Drawing.Size(1200, 877);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -275,7 +299,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Criteria";
